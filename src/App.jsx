4f9e35837d36a810b99c194c89e28995b87961c2
mayank-singh-ts <mayank.singh@tamarsoftware.in>
2024-12-13 21:39:22 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,7 @@ function App() {
 
     try {
       // Send user input to backend (Flask API)
-      const response = await axios.post('http://http://13.49.68.219:5001/query', {
+      const response = await axios.post('http://13.49.68.219:5001/query', {
         message: userInput,
         user_id: 'user1', // Use a fixed user ID for simplicity
       });
